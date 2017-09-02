c85430de1eafc24b34ab428dca87245b70c24e2f
jiyong <teran1024@naver.com>
2017-09-02 19:37:55 +0900

    파일 다운로드 방식 변경(컨트롤러가 아니면 DB에 삽입되지 않던 버그 수정) rs로 받아서 컨트롤러에서 DB에 삽입

--- erp/src/main/java/com/company/myapp/CommonCollectClass.java
+++ erp/src/main/java/com/company/myapp/CommonCollectClass.java
@@ -5,22 +5,19 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.mycompany.mapper.FileMapper;
 import com.mycompany.vo.FileForm;
 
+@Service
 public class CommonCollectClass {
 	static int cnt = 0;
-	
-	@Autowired
-	private FileMapper fileMapper;
 	
 //	private String windowUploadPath = "C:\\Spring\\upload\\";
 	private String commonUploadPath = "C:\\Spring\\upload\\";
@@ -83,7 +80,8 @@ public class CommonCollectClass {
 		return column;
 	}
 	
-	public boolean insertFileModule(int no, FileForm uploadForm) throws Exception{
+	public HashMap<String,String> insertFileModule(int no, FileForm uploadForm) throws IOException{
+		HashMap<String,String> rq = new HashMap<String,String>();
 		List<String> fileNames = new ArrayList<String>();
 		List<MultipartFile> files = uploadForm.getFiles(); //
 
@@ -110,7 +108,11 @@ public class CommonCollectClass {
 				m = p.matcher(fileName); // fileName에 정규식 적용
 				extension = "";
 //				String fileUploadPath = "/root/upload/";
-				fileUploadPath = getCommonUploadPath();
+				if(no==0){
+					fileUploadPath = getCommonUploadTempPath();
+				}else{
+					fileUploadPath = getCommonUploadPath();
+				}
 				// 디렉토리가 없으면 생성해준다
 				fileuploadPath = new File(fileUploadPath);
 				if(!fileuploadPath.exists()) fileuploadPath.mkdirs(); 
@@ -119,15 +121,11 @@ public class CommonCollectClass {
 					extension = m.group(2); // 확장자를 저장
 				}
 				if (Pattern.matches(extensionCheck, extension)) { // 확장자 검사를 만족하면 디비에 넣는다.
-					if (cnt != 0) // for문 안에 cnt값이 있으면 계속 0 으로 초기화 되기 때문에
-									// static으로 선언
+					if (cnt != 0) // for문 안에 cnt값이 있으면 계속 0 으로 초기화 되기 때문에 static으로 선언
 					sdfFiletoString += cnt; // 다중 업로드 시 파일 이름을 다르게 해준다.
-//					bookMapper.insertFiles(no, sdfFiletoString + "." + extension, fileName, uploadForm.getUpDir()); // 파일 디비 저장
-					try{
-						fileMapper.insertFiles(no, sdfFiletoString + "." + extension, fileName, fileUploadPath); // 파일 디비 저장
-					}catch(Exception e){
-						System.out.println(e.getMessage());
-					}
+					rq.put("save_name", sdfFiletoString + "." + extension);
+					rq.put("real_name", fileName);
+					rq.put("path", fileUploadPath);
 					cnt++; // 다중 업로드 인지 확인하는 부분.
 //					String path = uploadForm.getUpDir() + sdfFiletoString + "." + extension; // 실제 파일 저장 하는 부분. 경로+이름+확장자
 					path = fileUploadPath + sdfFiletoString + "." + extension; // 실제 파일 저장 하는 부분. 경로+이름+확장자
@@ -139,7 +137,6 @@ public class CommonCollectClass {
 				}
 			}
 		}
-		return true;
+		return rq; 
 	}
-	
 }
