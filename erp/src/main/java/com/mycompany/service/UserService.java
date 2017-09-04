ef2b7fc4529dc918e921310436b15a881119d78a
jiyong <teran1024@naver.com>
2017-09-04 10:32:42 +0900

    메인화면에서 시간이 나오는 형식 변경

--- erp/src/main/java/com/mycompany/service/UserService.java
+++
@@ -1,9 +0,0 @@
-//package com.mycompany.service;
-//
-//import com.mycompany.exception.EmailExistsException;
-//import com.mycompany.resource.User;
-//
-//public interface UserService {
-//	public void create(User user) throws EmailExistsException;
-//	public void addAuthority(String email, String authority);
-//}
