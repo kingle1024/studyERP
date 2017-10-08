e5b999b668901bc75fbb4637391189d1aec7ad0e
jiyong <teran1024@naver.com>
2017-10-08 22:28:01 +0900

    방문자 통계 morris 그래프 추가

--- erp/src/main/java/com/mycompany/mapper/CommonMapper.java
+++ erp/src/main/java/com/mycompany/mapper/CommonMapper.java
@@ -1,9 +1,17 @@
 package com.mycompany.mapper;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 public interface CommonMapper {
 	@Select("select name from users where email = #{email}")
 	public String getEmailFromUsers(@Param("email") String email);
+	
+	@Insert("INSERT INTO visit (V_DATE) VALUES (now())")
+	public boolean insertToday();
+	
+	@Select("SELECT COUNT(*) FROM visit WHERE V_DATE = #{V_DATE}")
+	public int getTodayCount(@Param("V_DATE") String V_DATE);
+	
 }
